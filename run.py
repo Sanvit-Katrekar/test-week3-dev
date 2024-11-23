755512afc8cd8214b785e37ae518c5c7c4db6ee7
sanvit <katrekarsanvit2@gmail.com>
2024-11-23 17:18:00 +0400

    hid API key

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import requests
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
 
 # Initialize the Flask app
 app = Flask(__name__, static_folder='app/static', template_folder='app/templates')
@@ -29,7 +36,20 @@ with app.app_context():
 @app.route('/')
 def index():
     comments = Comment.query.all()
-    return render_template('index.html', comments=[{'NAME': c.name, 'POSITION': c.position, 'COMMENT': c.comment} for c in comments])
+    category = 'computers'
+    api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    if response.status_code == requests.codes.ok:
+        quote = response.json()[0]['quote']
+    else:
+        print("Error:", response.status_code, response.text)
+        quote = 'Loading...'
+
+    return render_template(
+        'index.html',
+        comments=[{'NAME': c.name, 'POSITION': c.position, 'COMMENT': c.comment} for c in comments],
+        amazing_quote=quote
+    )
 
 # Route for handling the form submission
 @app.route('/add_comment', methods=['POST'])
